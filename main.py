13384e2eafe8fbfd250d47cff025c3c419669729
JLaumen <J.Laumen@student.science.ru.nl>
2023-04-12 23:23:14 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from tkinter import *
+from tkinter import ttk
+
+
+root = Tk()
+root.title("Sorting Visualiser")
+ttk.Label(root, text="Sorting Visualiser").grid()
+
+
+def main():
+    root.mainloop()
+
+
+if __name__ == '__main__':
+    main()
